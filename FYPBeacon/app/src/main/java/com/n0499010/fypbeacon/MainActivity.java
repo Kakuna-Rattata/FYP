34ba71d999b2e504f8aaa80901a55f7b60c5d1d4
Kakuna-Rattata <purplefuse@gmail.com>
2017-03-06 16:58:15 +0000

    Completed Estimote Android Tutorial, with Basic UI added

--- FYPBeacon/app/src/main/java/com/n0499010/fypbeacon/MainActivity.java
+++ FYPBeacon/app/src/main/java/com/n0499010/fypbeacon/MainActivity.java
@@ -2,15 +2,104 @@ package com.n0499010.fypbeacon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    //TODO: have the beacon manager in the application class manipulate a data model,
+    //TODO: and the Activity observing the changes to the data model instead.
+
+    // Declare UI elements :
+    private ListView mListView;
+
+    //static String[] itemArray = {"Pastel Heels", "Black Boots", "Pretty Bangle"};
+
+    private BeaconManager beaconManager;
+    private Region region;
+
+    private static final Map<String, List<String>> PLACES_BY_BEACONS;
+    private static ArrayList<String> itemArrayList = new ArrayList<String>();
+
+    /* List nested inside a map. The map maps beacon’s <major>:<minor> string) to
+     * the list of names of objects, pre-sorted by starting with the nearest ones. */
+    static {
+        Map<String, List<String>> itemsByBeacons = new HashMap<>();
+        itemsByBeacons.put("18129:1432", itemArrayList = new ArrayList<String>() {{
+            add("Pastel Heels");
+            // "Pastel Heels" is closest to the beacon with major 18129 and minor 1432
+            add("Black Boots");
+            // "Green & Green Salads" is the next closest
+            add("Pretty Bangle");
+            // "Mini Panini" is the furthest away
+        }});
+        itemsByBeacons.put("648:12", new ArrayList<String>() {{
+            add("Pastel Heels");
+            add("Black Boots");
+            add("Pretty Bangle");
+        }});
+        PLACES_BY_BEACONS = Collections.unmodifiableMap(itemsByBeacons);
+    }
+
+    /* takes a Beacon object representing the closest beacon, and
+     * return a list of all the objects sorted by their distance to the beacon */
+    private List<String> itemsNearBeacon(Beacon beacon) {
+
+        String beaconKey = String.format("%d:%d", beacon.getMajor(), beacon.getMinor());
+
+        if (PLACES_BY_BEACONS.containsKey(beaconKey)) {
+            return PLACES_BY_BEACONS.get(beaconKey);
+        }
+        return Collections.emptyList();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView_items);
+
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, itemArrayList);
+        mListView.setAdapter(adapter);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+        beaconManager = new BeaconManager(this);
+        region = new Region("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                null,   // Major and Minor values null to target all beacons
+                null);
+
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Beacon nearestBeacon = list.get(0); // List already ordered nearest first
+                    List<String> places = itemsNearBeacon(nearestBeacon);
+
+                    Log.d("Store", "Nearest places: " + places);
+
+                    // Update UI:
+                    adapter.clear();
+                    adapter.addAll(places);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
     @Override
@@ -20,5 +109,19 @@ public class MainActivity extends AppCompatActivity {
         /* The checkWithDefaultDialogs Helper method will use default system dialogs
         to turn Bluetooth or Location on, ask for ACCESS_COARSE_LOCATION permission. */
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
     }
 }
---
+++ FYPBeacon/app/src/main/java/com/n0499010/fypbeacon/MyApplication.java
@@ -0,0 +1,93 @@
+package com.n0499010.fypbeacon;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by N0499010 Shannon Hibbett on 06/03/2017
+ * Source Code extended from Estimote Android Tutorial (part 2) :
+ * http://developer.estimote.com/android/tutorial/part-2-background-monitoring/
+ */
+
+/* Application: Base class for those who need to maintain global application state.
+*  Required for managing Beacons from any Activity in the app. */
+public class MyApplication extends Application {
+
+    private BeaconManager beaconManager;
+    long scanDurInterval = 5000;
+    long scanWaitInterval = 5000;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.setBackgroundScanPeriod(scanDurInterval, scanWaitInterval);   // Set enter/exit event trigger duration and wait time to 5 seconds
+
+        // Create a beacon region defining monitoring geofence :
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region(
+                        "monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),    // Estimote 'Beetroot' Beacon Unique ID
+                        18129,  // iBeacon format Major value
+                        1432    //  Minor value
+                        //  Target entire groups of beacons by setting the major and/or minor to null.
+                ));
+            }
+        });
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                showNotification(
+                        "Welcome to the store", // Title
+                        "Check out the latest app-only instore offers."  // Message
+                );
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                showNotification(
+                        "Thank you for shopping with us",
+                        "Check back soon for the latest " +
+                                "app instore discounts"
+                );
+            }
+        });
+    }
+
+    /* Add a notification to show up whenever
+     * user enters the range of our monitored beacon. */
+    public void showNotification(String title, String message) {
+
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[] {notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+}
